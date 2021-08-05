a6d1d5db262bb7099a3f803d44d778d34c7c42fc
Black_glb <Black_glb@gmail.com>
2021-08-05 23:39:22 +0800

    master fouth commit

--- src/GitTest.java
+++ src/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
         System.out.println("Hello  git");
         System.out.println("fasdfawdfwdf");
         System.out.println("hello fix");
+        System.out.println("master Test");
     }
 }
