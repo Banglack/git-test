c919b8287295b18e25e2df870b26d785efe002d0
Black_glb <Black_glb@gmail.com>
2021-08-05 23:38:14 +0800

    hotfix second commit

--- src/GitTest.java
+++ src/GitTest.java
@@ -2,5 +2,6 @@ public class GitTest {
     public static void main(String[] args){
         System.out.println("Hello git");
         System.out.println("hello fix");
+        System.out.println("hot-fix Test");
     }
 }
